896137306291ca7b98bc94c3c8a212c398e513df
Navam <navamsharma142@gmail.com>
2026-01-06 09:31:02 +0530

    completed u5

--- src/BannerApplication.java
+++ src/BannerApplication.java
@@ -3,6 +3,7 @@ import u1.U1;
 import u2.U2;
 import u3.U3;
 import u4.U4;
+import u5.U5;
 
 public class BannerApplication {
     public static void main(String[] args) {
@@ -10,6 +11,6 @@ public class BannerApplication {
         U2.printOops();
         U3.printOops();
         U4.CharacterPatternMap.printOops();
-
+        U5.printOops();
     }
 }
